f31b3705374f0f51fe3df4347f135face680fa04
javagoza <enriquealbertos@hotmail.com>
2015-07-11 17:47:51 +0200

    Plot1
    First version of Plot1

---
+++ plot1.R
@@ -0,0 +1,38 @@
+# plot1.R
+plot1 <- function()
+  # download data
+  url <-
+    "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download(url, "exdata-data-household_power_consumption.zip", mode = "wb")
+  unlink(url)
+  
+  # and extract file
+  unzip("exdata-data-household_power_consumption.zip")
+  
+  fullData <-
+    read.table(
+      "household_power_consumption.txt", sep = ";", header = TRUE, na.strings =
+        "?",  stringsAsFactors = FALSE
+    )
+  fullData$DateTime <- paste(fullData$Date, fullData$Time)
+  # create date time column
+  fullData$DateTime <-
+    as.Date(fullData$DateTime, format = "%d/%m/%Y %H:%M:%S")
+  # filter data between "2007-02-01" and "2007-02-02"
+  data <-
+    DT[which((fullData$DateTime >= "2007-02-01") &
+               (fullData$DateTime < "2007-02-03")),]
+  # fee space
+  rm(fullData)
+  
+  
+  # plot histogram
+  windows()
+  with(
+    data, hist(
+      Global_active_power, main = "Global Active Power", col = "red" , xlab = "Global Active Power (kiloWatts)"
+    )
+  )
+  dev.copy(png, file = "plot1.png", width = 480, height = 480)
+  dev.off()
+  
